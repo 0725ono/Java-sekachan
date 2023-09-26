21b1cbcd5814f36270f90fe61112583063d5f344
Masa Ono <ono.deutsch@gmail.com>
2023-09-26 11:14:51 +0900

    最初のコミット

---
+++ javapractice-sekachan/src/Triangle.java
@@ -0,0 +1,19 @@
+
+public class Triangle {
+
+	public static void main(String[] args) {
+		int height = 5;
+		int width = 10;
+		int area;
+		
+		//面積の計算
+		area = height * width / 2;
+		
+		//高さ・幅・面積の表示
+		System.out.println("高さ：　" + height);
+		System.out.println("幅　：　" + width);
+		System.out.println("面積：　" + area);
+		
+	}
+
+}
